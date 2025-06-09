dc3c823449b8795f93967c56a0400566fa11e3a6
sheep289 <2890492738@qq.com>
2025-06-10 03:34:04 +0800

    fix: update mysql config

--- db/index.js
+++ db/index.js
@@ -6,9 +6,11 @@ const db = mysql.createPool({
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
-    connectionLimit: 10,  //限制最大连数
+    port:20497,
+    connectionLimit: 30,  //限制最大连数
     connectTimeout: 10000,   //10秒连接超时
     queueLimit: 0,    //不限制排队请求
+    ssl: { rejectUnauthorized: false }  // Railway强制SSL
 })
 
 module.exports = db
